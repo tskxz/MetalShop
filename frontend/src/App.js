a60d41858249c34c8381775d1573e768a4b56e89
Tanjil <tanjilkh@gmail.com>
2024-08-06 13:53:40 +0100

    Update App.js
    incluir componente header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 import React from 'react'
 
 const App = () => {
-  return (
-   <h1>Bem vindo ao MetalShop</h1>
+  return ( 
+   <>
+   <Header/>
+   <main className="py-3">
+      <Container> 
+        <h1>Bem vindo ao MetalShop</h1>
+      </Container>
+   </main>
+   </>
   )
 }
 

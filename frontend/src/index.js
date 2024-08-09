4da4bd09d517fc426317a98e5ef495dffde28889
Tanjil <tanjilkh@gmail.com>
2024-08-09 10:34:09 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,11 +13,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProdutoScreen from './screens/ProdutoScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<HomeScreen/>} /> 
+      <Route path="/produto/:id" element={<ProdutoScreen/>} /> 
     </Route>
   )
 )

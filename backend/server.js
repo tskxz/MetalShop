b29b361975a21e59a7c4602c780481ec5ab0c88b
Tanjil <tanjilkh@gmail.com>
2024-08-13 12:06:44 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+import express from 'express';
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API está a rodar...');
+});
+
+app.listen(port, () => console.log(`Servidor a rodar na porta ${port}`))

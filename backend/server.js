d3fb48aae37f270c9182dd136e471d1cbb458568
Tanjil <tanjilkh@gmail.com>
2024-08-13 12:18:39 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import produtos from './data/produtos.js';
+
 const port = 5000;
 
 const app = express();
@@ -6,5 +8,14 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('API está a rodar...');
 });
+
+app.get('/api/produtos', (req, res) => {
+    res.json(produtos)
+})
+
+app.get('/api/produtos/:id', (req, res) => {
+    const produto = produtos.find((p) => p._id === req.params.id);
+    res.json(produto)
+})
 
 app.listen(port, () => console.log(`Servidor a rodar na porta ${port}`))

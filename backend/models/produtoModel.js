c3e3b07260041f65d2d0361f13d40d9358432955
Tanjil <tanjilkh@gmail.com>
2024-08-13 15:02:56 +0100

    Create produtoModel.js

---
+++ backend/models/produtoModel.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+
+const produtoSchema = new mongoose.Schema({
+
+    utilizador: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Utilizador"
+    },
+
+    nome: {
+        type: String,
+        required: true,
+    },
+
+    imagem: {
+        type: String,
+        required: true,
+    },
+
+    banda: {
+        type: String,
+        required: true,
+    },
+
+    categoria: {
+        type: String,
+        required: true,
+    },
+
+    descricao: {
+        type: String,
+        required: true,
+    },
+
+    genero: {
+        type: String,
+        required: true,
+    },
+    cor: {
+        type: String,
+        required: true
+    },
+    tamanho: {
+        type: String,
+        required: true,
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    reviews: [reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    preco: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    emStock: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+
+
+}, {timestamps: true})
+
+const Produto = mongoose.model("Produto", produtoSchema);
+export default Produto;

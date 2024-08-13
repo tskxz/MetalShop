5b02ecbd6b620d00549db3d3f8eb2f02b1a2870e
Tanjil <tanjilkh@gmail.com>
2024-08-13 13:10:13 +0100

    Update HomeScreen.jsx

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import React from 'react'
 import {Row, Col} from 'react-bootstrap'
-import produtos from '../produtos.js'
 import Produto from '../components/Produto.jsx'
 
 const HomeScreen = () => {
+
+  const [produtos, setProdutos] = useState([]);
+  
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      const {data} = await axios.get('/api/produtos');
+      setProdutos(data)
+    };
+
+    fetchProdutos();
+  }, [])
+
   return (
     <>
         <h1>Os Nossos Produtos</h1>

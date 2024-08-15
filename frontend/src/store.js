c181189168bfba86db6681b54d0716bd77672a31
Tanjil <tanjilkh@gmail.com>
2024-08-15 14:28:53 +0100

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {apiSlice} from './slices/apiSlice'
+import carrinhoSliceReducer from './slices/carrinhoSlice'
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        carrinho: carrinhoSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,

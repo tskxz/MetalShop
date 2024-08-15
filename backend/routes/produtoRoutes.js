2f3836d95ea31e451f5a4c1ef12c158e8cbc97ca
Tanjil <tanjilkh@gmail.com>
2024-08-15 11:59:18 +0100

    Update produtoRoutes.js

--- backend/routes/produtoRoutes.js
+++ backend/routes/produtoRoutes.js
@@ -1,22 +1,8 @@
 import express from 'express'
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Produto from '../models/produtoModel.js'
+import {getProdutos, getProduto} from '../controllers/produtoController.js'
 
-router.get('/', asyncHandler (async(req, res) => {
-    const produtos = await Produto.find({});
-    res.json(produtos)
-}))
-
-router.get('/:id', asyncHandler( async(req, res) => {
-    const produto = await Produto.findById(req.params.id);
-    if(produto){
-        res.json(produto)
-    } else {
-        res.status(404)
-        throw new Error('Produto nao encontrado')
-    }
-   
-}))
+router.route('/').get(getProdutos);
+router.route('/:id').get(getProduto);
 
 export default router

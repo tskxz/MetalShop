2d0e4e65d67da5aaf9bcc2d1e795e3f8e5182e81
Tanjil <tanjilkh@gmail.com>
2024-08-15 10:37:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ dotenv.config();
 import produtos from './data/produtos.js';
 import connectDB from './config/db.js';
 
+import produtoRoutes from './routes/produtoRoutes.js'
+
 const port = process.env.PORT || 5000;
 
 connectDB(); // Conexão da base de dados
@@ -14,13 +16,6 @@ app.get('/', (req, res) => {
     res.send('API está a rodar...');
 });
 
-app.get('/api/produtos', (req, res) => {
-    res.json(produtos)
-})
-
-app.get('/api/produtos/:id', (req, res) => {
-    const produto = produtos.find((p) => p._id === req.params.id);
-    res.json(produto)
-})
+app.use('/api/produtos', produtoRoutes);
 
 app.listen(port, () => console.log(`Servidor a rodar na porta ${port}`))

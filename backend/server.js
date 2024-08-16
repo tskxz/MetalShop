e9a667af6cef6c2a4fe0ea850b986327d09e2995
Tanjil <tanjilkh@gmail.com>
2024-08-16 14:15:54 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import cookieParser from 'cookie-parser'
 
 import produtoRoutes from './routes/produtoRoutes.js'
 import utilizadorRoutes from './routes/utilizadorRoutes.js'
@@ -16,6 +17,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API está a rodar...');

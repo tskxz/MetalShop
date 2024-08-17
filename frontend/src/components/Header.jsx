c25e76c89cb3216de336ce2a8af19afc93b12f64
Tanjil <tanjilkh@gmail.com>
2024-08-17 16:02:23 +0100

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Badge, Navbar, Nav, Container} from 'react-bootstrap';
+import {Badge, Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa'; 
 import {LinkContainer} from 'react-router-bootstrap';
 import {useSelector} from 'react-redux'
 
 const Header = () => {
     const {carrinhoItens} = useSelector((state) => state.carrinho)
+    const {utilizadorInfo} = useSelector((state) => state.auth)
+    const logoutHandler = () => {
+        console.log('logout')
+    }
     console.log(carrinhoItens)
   return (
     <header>
@@ -27,9 +31,16 @@ const Header = () => {
                                }
                             </Nav.Link>
                         </LinkContainer>
-                        <LinkContainer to='login'>
+                        {utilizadorInfo ? (
+                            <NavDropdown title={utilizadorInfo.nome} id='username'>
+                                <LinkContainer to='/perfil'>
+                                    <NavDropdown.Item>Perfil</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>):( <LinkContainer to='login'>
                             <Nav.Link><FaUser/> Entrar </Nav.Link>
-                        </LinkContainer>
+                        </LinkContainer>)}
+                       
                     </Nav>
                 </Navbar.Collapse>
             </Container>

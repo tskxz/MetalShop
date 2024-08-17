67e0a9094a95a1b1de7fe130c80bfdb738ed7db9
Tanjil <tanjilkh@gmail.com>
2024-08-17 14:17:53 +0100

    Create utilizadoresApiSlice.js

---
+++ frontend/src/slices/utilizadoresApiSlice.js
@@ -0,0 +1,17 @@
+import {UTILIZADORES_URL} from '../constants.js'
+import {apiSlice} from './apiSlice.js'
+
+export const utilizadoresApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: UTILIZADORES_URL/login,
+                method: 'POST',
+                body: data,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+    })
+})
+
+export const { useLoginMutation } = utilizadoresApiSlice

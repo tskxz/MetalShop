b9bdbab53d9c093dace6e1d8de78a04f035308c0
Tanjil <tanjilkh@gmail.com>
2024-08-17 15:59:07 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {Outlet} from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import React from 'react'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return ( 
@@ -14,6 +16,7 @@ const App = () => {
       </Container>
    </main>
    <Footer/>
+   <ToastContainer/>
    </>
   )
 }

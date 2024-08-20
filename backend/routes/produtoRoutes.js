af47d8fc512af00da682dee9e460811e7536805b
Tanjil <tanjilkh@gmail.com>
2024-08-20 14:44:28 +0100

    Update produtoRoutes.js

--- backend/routes/produtoRoutes.js
+++ backend/routes/produtoRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router();
-import {getProdutos, getProduto} from '../controllers/produtoController.js'
+import {getProdutos, getProduto, criarProduto} from '../controllers/produtoController.js'
+import {protect, admin} from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProdutos);
+router.route('/').get(getProdutos).post(protect, admin, criarProduto);
 router.route('/:id').get(getProduto);
 
 export default router

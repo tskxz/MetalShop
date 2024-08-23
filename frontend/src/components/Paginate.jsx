4b273c13a287fd8668d08574656f20c63e4bb244
Tanjil <tanjilkh@gmail.com>
2024-08-23 11:42:45 +0100

    Update Paginate.jsx

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,12 +1,12 @@
 import {Pagination} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 
-const Paginate = ({pages, page, isAdmin = false}) => {
+const Paginate = ({pages, page, isAdmin = false, keyword=''}) => {
 	return(
 		pages > 1 && (
 			<Pagination>
 				{[...Array(pages).keys()].map((x) => (
-					<LinkContainer key={x+1} to={!isAdmin ? `/page/${x+1}` : `/admin/listaproduto/${x+1}`}>
+					<LinkContainer key={x+1} to={!isAdmin ? keyword? `/search/${keyword}/page/${x+1}` : `/page/${x+1}` : `/admin/listaproduto/${x+1}`}>
 						<Pagination.Item active={x+1 === page}>{x+1}</Pagination.Item>
 					</LinkContainer>
 				))}

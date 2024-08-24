1991f9aaa399f4aa346425f8733e6e173b333d1f
Tanjil <tanjilkh@gmail.com>
2024-08-24 11:11:56 +0100

    Update HomeScreen.jsx

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,13 +7,14 @@ import Message from '../components/Message.jsx'
 import {useParams} from 'react-router-dom'
 import Paginate from '../components/Paginate.jsx'
 import {Link} from 'react-router-dom'
+import ProdutoCarousel from '../components/ProdutoCarousel'
 
 const HomeScreen = () => {
     const {pageNumber, keyword} = useParams()
   const {data, isLoading, error} = useGetProdutosQuery({keyword, pageNumber});
   return (
     <>
-    {keyword && <Link to='/' className='btn btn-light mb-4'> Go back </Link>}
+    {!keyword ? <ProdutoCarousel/> : (<Link to='/' className='btn btn-light mb-4'> Go back </Link>)}
     {isLoading ? (<Loader/>) : error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (<>
        <h1>Os Nossos Produtos</h1>
        <Row>
